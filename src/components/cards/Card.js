3f6486c64750489f196eaec5cfbbc44205c49f99
ridhoajibx <ridhoajibx@gmail.com>
2020-10-01 09:16:03 +0700

    [ridho]--try to get API

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Card.css';
+import ReactStars from "react-rating-stars-component";
+
 
 const Card = (props) => {
+    const rating = {
+        size : 20,
+        value : `${props.rating}` / 2,
+        edit: false
+    }
     return (
             <div className="cardMovie">
                 <Link to="">
-                    <div className="img1" style={{ backgroundImage: `url(${props.content.imageUrl})` }}></div>
-                    <div className="title">{props.content.title}</div>
-                    <div className="text"> { props.content.text } </div>
-                    <Link to="/"><div className="catagory">{ props.content.category } <i className="fas fa-film"></i></div></Link>
-                    <Link to="/"><div className="views">{ props.content.date }  <i className="far fa-eye"></i> </div></Link>
+                    <div className="img1" style={{ backgroundImage: `url(${props.imageUrl})` }}></div>
+                    <div className="title">{props.title}</div>
+                    <div className="text"> { props.text } </div>
+                    <Link to="/"><div className="catagory">{props.lang} <i className="fas fa-film"></i></div></Link>
+                    <Link to="/"><div className="views">{props.date}  <i className="far fa-eye"></i> </div></Link>
+                    <div className="rating d-flex align-items-center">
+                        <span className="mr-2">{ props.rating }</span> <ReactStars {...rating}/>
+                    </div>
                 </Link>
             </div>
     );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Card from '../../components/cards/Card';
+
 import Carousel from '../../components/carousel/Carousel';
+import MovieIndex from '../movies/Index';
 import Footer from '../../components/footer/Footer';
 import NavPills from '../../components/navpills/NavPills';
 import Pagination from '../../components/pagination/Pagination';
 
 const Home = () => {
+    
     return (
         <div>
             <Carousel />
@@ -20,87 +22,8 @@ const Home = () => {
 
             <section>
                 <div className="container">
-                    <div className="row">
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-
-                        <Card content={{
-                            imageUrl: "http://cima4u.tv/wp-content/uploads/1-71.jpg",
-                            title: "Vikings",
-                            category: "Action",
-                            text: "Now You can watch the first epsoide of the action series Vikings , wish you interesting watch",
-                            date: "2020"
-                        }} />
-                    </div>
+                    <MovieIndex />
+                    
                     <div className="row">
                         <div className="col d-flex justify-content-center">
                             <Pagination />
