3e97fb21014e0430789d024582f250e4d8650447
ridhoajibx <ridhoajibx@gmail.com>
2020-10-08 13:28:51 +0700

    [ridho]--test rename branch

--- src/pages/movies/detailMovies/components/Overview.js
+++ src/pages/movies/detailMovies/components/Overview.js
@@ -7,7 +7,7 @@ const Overview = () => {
                 <h4 className="font-weight-bold">Synopsis</h4>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugit quas qui tempore pariatur praesentium dolores voluptatum aut blanditiis est, consequatur enim. Minima, officia dolor! Veritatis, numquam consequuntur. Id accusantium voluptate modi repellendus unde voluptates ipsam nisi obcaecati libero, et, voluptatum saepe laudantium distinctio dolor aliquam odit atque vitae consectetur?
-
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore placeat sint illum obcaecati consequuntur corporis corrupti in voluptate, nisi atque laboriosam natus culpa magni sunt nihil iure! Qui, quidem nobis.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugit quas qui tempore pariatur praesentium dolores voluptatum aut blanditiis est, consequatur enim. Minima, officia dolor! Veritatis, numquam consequuntur. Id accusantium voluptate modi repellendus unde voluptates ipsam nisi obcaecati libero, et, voluptatum saepe laudantium distinctio dolor aliquam odit atque vitae consectetur?
                 </p>
             </div>
